4f40ec3b85a6260b20e1a40c6fd011aa9ccfa7c7
Lanf0rd <aleks8423@mail.ru>
2022-12-22 20:20:36 +0300

    lab2 v0.0.1 was added

---
+++ lab2/Gateway/main.py
@@ -0,0 +1,93 @@
+import flask
+
+
+
+
+
+class Server:
+    def __init__(self, host, port, tickets_port, flights_port, bonuses_port):
+        self.host = host
+        self.port = port
+        self.Tickets = tickets_port
+        self.Flights = flights_port
+        self.Bonuses = bonuses_port
+        self.app = flask.Flask(__name__)
+        
+        self.app.add_url_rule('/api/v1/flights', view_func = self.get_flights)
+        self.app.add_url_rule('/api/v1/tickets', view_func = self.get_tickets)
+        self.app.add_url_rule('/api/v1/tickets', view_func = self.post_tickets, methods = ['POST'])
+        self.app.add_url_rule('/api/v1/tickets/<ticketUid>', view_func = self.get_tickets_by_id)
+        self.app.add_url_rule('/api/v1/tickets/<ticketUid>', view_func = self.delete_tickets_by_id, methods = ['DELETE'])
+        self.app.add_url_rule('/api/v1/me', view_func = self.get_me)
+        self.app.add_url_rule('/api/v1/privilege', view_func = self.get_privelege)
+
+    def run_server(self):
+        return self.app.run(host = self.host, port = self.port)
+
+
+    def get_flights(self):
+        return "get flights"
+
+    def get_tickets(self):
+        return "get tickets"
+
+    def post_tickets(self):
+        return "post tickets"
+
+    def get_tickets_by_id(self, ticketUid):
+        return "get tickets by uid " + ticketUid
+
+    def delete_tickets_by_id(self, ticketUid):
+        return "delete tickets by uid " + ticketUid
+
+    def get_me(self):
+        return "Its my data"
+
+    def get_privelege(self):
+        return "Its my prevelege"
+
+
+
+if __name__ == "__main__":
+
+    server_host = '0.0.0.0'
+    server_port = 8080
+    tickets_port = 8070
+    flights_port = 8060
+    bonuses_port = 8050
+
+    server = Server(server_host, server_port, tickets_port, flights_port, bonuses_port)
+    server.run_server()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
